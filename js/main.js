05ac51d28763a662bc0a8a0657892bda32f19459
mahmud009 <mahmud.est@gmail.com>
2020-03-18 02:28:06 +0600

    added animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 $(function() {
+  // Initializing AOS
+  AOS.init();
+
+  //Navbar toggler class for responsive menu
   $(".cs-nav-tog").on("click", function() {
     $(".navbar").toggleClass("nav-opened");
   });
 
+  $(".navbar .nav-link").on("click", function() {
+    $(".navbar").removeClass("nav-opened");
+  });
+
+  //Header scroll behavior fix
   $(window).on("scroll", function(e) {
     if ($(window).scrollTop() > 300) {
       $(".navbar").addClass("nav-scrolled");
@@ -10,9 +19,5 @@ $(function() {
     } else {
       $(".navbar").removeClass("nav-scrolled");
     }
-  });
-
-  $(".navbar .nav-link").on("click", function() {
-    $(".navbar").removeClass("nav-opened");
   });
 });
